8dd1d04d369021872a1e1bed0b94f4eadada1d16
Damdadi93 <swoo567@naver.com>
2022-12-14 17:54:07 +0900

    221214
    기본움직임 애니매이션 구현

--- Assets/Sangwoo/s_Script/Animation/BagicMovement.cs
+++ Assets/Sangwoo/s_Script/Animation/BagicMovement.cs
@@ -1,27 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class BagicMovement : MonoBehaviour
 {
-   
+    Rigidbody Rigid;
+    Vector3 Movement;
     private Animator animator;
-    public float speed;
+    public float Speed;
+    public float rotateSpeed;
+    float h, v;
+    float lastAttackTime = 0f;
+    
+   
+    
 
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        Rigid = GetComponent<Rigidbody>();
     }
    
 
    
-    private void Update()
+    void Update()
     {
-        float MoveX = Input.GetAxis("Horizontal");
-        float MoveY = Input.GetAxis("Vertical");
+        //애니메
+        Move();
 
-        animator.SetFloat("MoveX", MoveX);
-        animator.SetFloat("MoveY", MoveY);
     }
+
+   
+
+    void Move()
+    {
+        //애니메
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        animator.SetFloat("MoveX", h);
+        animator.SetFloat("MoveY", v);
+
+        transform.position += new Vector3(h, 0, v) * Speed * Time.deltaTime;
+
+
+        //캐릭터 얼굴 방향
+        Vector3 dir = new Vector3(h, 0, v);
+
+        if(!(h == 0 && v == 0))
+        {
+            transform.position += dir * Speed * Time.deltaTime;
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * rotateSpeed);
+        }
+
+        //Movement.Set(h, 0, v);
+       // Movement = Movement.normalized * speed * Time.deltaTime;
+
+
+        //rigidbody.MovePosition(transform.position + Movement);
+    }
+
+    //private void OnAnimatorMove() 뭘까?
+  
 }
