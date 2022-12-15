60cda78cda4e242f0284f2a9c0f42c2a0ddd4812
LostMoa <life4458@naver.com>
2022-12-16 05:36:56 +0900

    20221216 update
    단축키 설정 관련 수정

--- Assets/Scripts/DataScripts/KeySet.cs
+++ Assets/Scripts/DataScripts/KeySet.cs
@@ -7,27 +7,36 @@ using UnityEngine.InputSystem.LowLevel;
 [CreateAssetMenu(fileName = "KeySet",menuName = "KeySet")]
 public class KeySet : ScriptableObject
 {
-    public enum MOVE_CODE { UP, DOWN, LEFT, RIGHT }
-
     [Header("Character Move Up/Down/Left/Right")]
-    [SerializeField] KeyCode[] move = new KeyCode[4] { KeyCode.W, KeyCode.S, KeyCode.A, KeyCode.D }; // up down left right
+    [SerializeField] KeyCode[] move = new KeyCode[KeyManager.moveSize]
+        { KeyCode.W, KeyCode.S, KeyCode.A, KeyCode.D }; // UP DOWN LEFT RIGHT
+    public KeyCode MoveUp { get { return move[(int)KeyManager.MOVE.UP]; } }
+    public KeyCode MoveDown { get { return move[(int)KeyManager.MOVE.DOWN]; } }
+    public KeyCode MoveLeft { get { return move[(int)KeyManager.MOVE.LEFT]; } }
+    public KeyCode MoveRight { get { return move[(int)KeyManager.MOVE.RIGHT]; } }
 
     [Header("Character Control")]
-    [SerializeField] KeyCode attack = KeyCode.Mouse0;
-    [SerializeField] KeyCode run = KeyCode.LeftControl;
-    [SerializeField] KeyCode dash = KeyCode.LeftShift;
-    [SerializeField] KeyCode jump = KeyCode.Space;
+    [SerializeField] KeyCode[] characterControl = new KeyCode[KeyManager.characterControlSize]
+        { KeyCode.Mouse0,KeyCode.LeftShift,KeyCode.LeftControl,KeyCode.Space };
+    public KeyCode Attack { get { return characterControl[(int)KeyManager.CHARACTER_CONTROL.ATTACK]; } }
+    public KeyCode Run { get { return characterControl[(int)KeyManager.CHARACTER_CONTROL.RUN]; } }
+    public KeyCode Dash { get { return characterControl[(int)KeyManager.CHARACTER_CONTROL.DASH]; } }
+    public KeyCode Jump { get { return characterControl[(int)KeyManager.CHARACTER_CONTROL.JUMP]; } }
 
     [Header("Normal Control")]
-    [SerializeField] KeyCode interaction = KeyCode.F;
-    [SerializeField] KeyCode sightRotation = KeyCode.Mouse1;
-    [SerializeField] KeyCode option = KeyCode.Escape; // optionWindow + closeWindow
-    [SerializeField] KeyCode inventory = KeyCode.I;
-    [SerializeField] KeyCode characterInfo = KeyCode.P;
-    [SerializeField] KeyCode skill = KeyCode.K;
-    [SerializeField] KeyCode quest = KeyCode.O;
+    [SerializeField] KeyCode[] normalControl = new KeyCode[KeyManager.normalControlSize]
+        { KeyCode.F,KeyCode.Mouse1,KeyCode.Escape,KeyCode.I,KeyCode.P,KeyCode.K,KeyCode.J };
+    public KeyCode Interaction { get { return normalControl[(int)KeyManager.NORMAL_CONTROL.INTERACTION]; } }
+    public KeyCode SightRotation { get { return normalControl[(int)KeyManager.NORMAL_CONTROL.SIGHT_ROTATION]; } }
+    public KeyCode Option { get { return normalControl[(int)KeyManager.NORMAL_CONTROL.OPTION]; } }
+    public KeyCode Inventory { get { return normalControl[(int)KeyManager.NORMAL_CONTROL.INVENTORY]; } }
+    public KeyCode CharacterInfo { get { return normalControl[(int)KeyManager.NORMAL_CONTROL.CHARACTER_INFO]; } }
+    public KeyCode Skill { get { return normalControl[(int)KeyManager.NORMAL_CONTROL.SKILL]; } }
+    public KeyCode Quest { get { return normalControl[(int)KeyManager.NORMAL_CONTROL.QUEST]; } }
 
     [Header("Quick Slot")]
     [SerializeField] KeyCode[] potionSlots = new KeyCode[4] { KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4 };
     [SerializeField] KeyCode[] skillSlots = new KeyCode[4] { KeyCode.Z, KeyCode.X, KeyCode.C, KeyCode.E };
+    public KeyCode[] PotionSlots { get { return potionSlots; } }
+    public KeyCode[] SkillSlots { get { return SkillSlots; } }
 }
--- Assets/Scripts/KeyManager.cs
+++ Assets/Scripts/KeyManager.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class KeyManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public const int moveSize = 4;
+    public const int characterControlSize = 4;
+    public const int normalControlSize = 7;
+
+    public static KeyManager instance;
+    [SerializeField] KeySet basicKeySet;
+
+    // 캐릭터 이동
+    public enum MOVE { UP, DOWN, LEFT, RIGHT }
+    KeyCode[] move;
+    // 캐릭터 조작
+    public enum CHARACTER_CONTROL { ATTACK, RUN, DASH, JUMP }
+    KeyCode[] characterControl;
+    // 일반 조작
+    public enum NORMAL_CONTROL { INTERACTION, SIGHT_ROTATION, OPTION, INVENTORY, CHARACTER_INFO, SKILL, QUEST }
+    KeyCode[] normalControl;
+    // 퀵슬롯
+    KeyCode[] potionSlots;
+    KeyCode[] skillSlots;
+
+    /* 1. 다른 스크립트에서 KeyManager.instance.~ 으로 키를 불러올 수 있어야함
+     * 
+     */
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        ResetAllKey();
     }
 
-    // Update is called once per frame
-    void Update()
+    void ResetAllKey()
     {
-        
+        for (int i = 0; i < moveSize; i++)
+        {
+        }
+        for (int i = 0; i < characterControlSize; i++)
+        {
+        }
+        for (int i = 0; i < normalControlSize; i++)
+        {
+        }
+        for (int i = 0; i < potionSlots.Length; i++)
+        {
+        }
+        for (int i = 0; i < skillSlots.Length; i++)
+        {
+        }
     }
 }
