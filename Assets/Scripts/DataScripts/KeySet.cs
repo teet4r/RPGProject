86e745c4bcb40ec405aaa27b887b1636c4024f13
LostMoa <life4458@naver.com>
2022-12-18 02:09:10 +0900

    20221218 update
    keymanager 수정

--- Assets/Scripts/DataScripts/KeySet.cs
+++ Assets/Scripts/DataScripts/KeySet.cs
@@ -2,41 +2,37 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem.LowLevel;
 
 [CreateAssetMenu(fileName = "KeySet",menuName = "KeySet")]
 public class KeySet : ScriptableObject
 {
+    // 작성자 : 김두현
     [Header("Character Move Up/Down/Left/Right")]
     [SerializeField] KeyCode[] move = new KeyCode[KeyManager.moveSize]
         { KeyCode.W, KeyCode.S, KeyCode.A, KeyCode.D }; // UP DOWN LEFT RIGHT
-    public KeyCode MoveUp { get { return move[(int)KeyManager.MOVE.UP]; } }
-    public KeyCode MoveDown { get { return move[(int)KeyManager.MOVE.DOWN]; } }
-    public KeyCode MoveLeft { get { return move[(int)KeyManager.MOVE.LEFT]; } }
-    public KeyCode MoveRight { get { return move[(int)KeyManager.MOVE.RIGHT]; } }
+    public KeyCode[] Move { get { return move; } }
 
     [Header("Character Control")]
     [SerializeField] KeyCode[] characterControl = new KeyCode[KeyManager.characterControlSize]
         { KeyCode.Mouse0,KeyCode.LeftShift,KeyCode.LeftControl,KeyCode.Space };
-    public KeyCode Attack { get { return characterControl[(int)KeyManager.CHARACTER_CONTROL.ATTACK]; } }
-    public KeyCode Run { get { return characterControl[(int)KeyManager.CHARACTER_CONTROL.RUN]; } }
-    public KeyCode Dash { get { return characterControl[(int)KeyManager.CHARACTER_CONTROL.DASH]; } }
-    public KeyCode Jump { get { return characterControl[(int)KeyManager.CHARACTER_CONTROL.JUMP]; } }
+    public KeyCode[] CharacterControl { get { return characterControl; } }
 
     [Header("Normal Control")]
     [SerializeField] KeyCode[] normalControl = new KeyCode[KeyManager.normalControlSize]
         { KeyCode.F,KeyCode.Mouse1,KeyCode.Escape,KeyCode.I,KeyCode.P,KeyCode.K,KeyCode.J };
-    public KeyCode Interaction { get { return normalControl[(int)KeyManager.NORMAL_CONTROL.INTERACTION]; } }
-    public KeyCode SightRotation { get { return normalControl[(int)KeyManager.NORMAL_CONTROL.SIGHT_ROTATION]; } }
-    public KeyCode Option { get { return normalControl[(int)KeyManager.NORMAL_CONTROL.OPTION]; } }
-    public KeyCode Inventory { get { return normalControl[(int)KeyManager.NORMAL_CONTROL.INVENTORY]; } }
-    public KeyCode CharacterInfo { get { return normalControl[(int)KeyManager.NORMAL_CONTROL.CHARACTER_INFO]; } }
-    public KeyCode Skill { get { return normalControl[(int)KeyManager.NORMAL_CONTROL.SKILL]; } }
-    public KeyCode Quest { get { return normalControl[(int)KeyManager.NORMAL_CONTROL.QUEST]; } }
-
+    public KeyCode[] NormalControl { get { return normalControl; } }
     [Header("Quick Slot")]
     [SerializeField] KeyCode[] potionSlots = new KeyCode[4] { KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4 };
     [SerializeField] KeyCode[] skillSlots = new KeyCode[4] { KeyCode.Z, KeyCode.X, KeyCode.C, KeyCode.E };
     public KeyCode[] PotionSlots { get { return potionSlots; } }
-    public KeyCode[] SkillSlots { get { return SkillSlots; } }
+    public KeyCode[] SkillSlots { get { return skillSlots; } }
+
+    public void SetKey(KeyCode[] _move, KeyCode[] _char, KeyCode[] _norm, KeyCode[] _potion, KeyCode[] _skill)
+    {
+        move = _move;
+        characterControl = _char;
+        normalControl = _norm;
+        potionSlots = _potion;
+        skillSlots = _skill;
+    }
 }
--- Assets/Scripts/KeyManager.cs
+++ Assets/Scripts/KeyManager.cs
@@ -4,29 +4,33 @@ using UnityEngine;
 
 public class KeyManager : MonoBehaviour
 {
+    // 작성자 : 김두현
     public const int moveSize = 4;
     public const int characterControlSize = 4;
     public const int normalControlSize = 7;
 
     public static KeyManager instance;
     [SerializeField] KeySet basicKeySet;
+    [SerializeField] KeySet userKeySet;
 
     // 캐릭터 이동
     public enum MOVE { UP, DOWN, LEFT, RIGHT }
-    KeyCode[] move;
+    KeyCode[] moveKeys = new KeyCode[moveSize];
+    public KeyCode[] MoveKeys { get { return moveKeys; } }
     // 캐릭터 조작
     public enum CHARACTER_CONTROL { ATTACK, RUN, DASH, JUMP }
-    KeyCode[] characterControl;
+    KeyCode[] characterControlKeys = new KeyCode[characterControlSize];
+    public KeyCode[] CharacterKeys { get { return characterControlKeys; } }
     // 일반 조작
     public enum NORMAL_CONTROL { INTERACTION, SIGHT_ROTATION, OPTION, INVENTORY, CHARACTER_INFO, SKILL, QUEST }
-    KeyCode[] normalControl;
+    KeyCode[] normalControlKeys = new KeyCode[normalControlSize];
+    public KeyCode[] NormalKeys { get { return normalControlKeys; } }
     // 퀵슬롯
-    KeyCode[] potionSlots;
-    KeyCode[] skillSlots;
+    KeyCode[] potionSlotsKeys;
+    public KeyCode[] PotionKeys { get { return potionSlotsKeys; } }
+    KeyCode[] skillSlotsKeys;
+    public KeyCode[] SkillKeys { get { return skillSlotsKeys; } }
 
-    /* 1. 다른 스크립트에서 KeyManager.instance.~ 으로 키를 불러올 수 있어야함
-     * 
-     */
     private void Awake()
     {
         if (instance == null)
@@ -43,20 +47,45 @@ public class KeyManager : MonoBehaviour
 
     void ResetAllKey()
     {
+        moveKeys = basicKeySet.Move;
+        characterControlKeys = basicKeySet.CharacterControl;
+        normalControlKeys = basicKeySet.NormalControl;
+        potionSlotsKeys = basicKeySet.PotionSlots;
+        skillSlotsKeys = basicKeySet.SkillSlots;
+    }
+
+    public void SaveKeySet()
+    {
+        userKeySet.SetKey(moveKeys, characterControlKeys, normalControlKeys, potionSlotsKeys, skillSlotsKeys);
+    }
+
+    bool IsDuplicated(KeyCode _keycode) // 중복체크 함수 true - 중복 false - 미중복
+    {
         for (int i = 0; i < moveSize; i++)
         {
+            if (_keycode == moveKeys[i]) return true;
         }
-        for (int i = 0; i < characterControlSize; i++)
+        for(int i=0;i<characterControlSize;i++)
         {
+            if (_keycode == characterControlKeys[i]) return true;
         }
         for (int i = 0; i < normalControlSize; i++)
         {
+            if (_keycode == normalControlKeys[i]) return true;
         }
-        for (int i = 0; i < potionSlots.Length; i++)
+        for(int i=0;i<potionSlotsKeys.Length;i++)
         {
+            if (_keycode == potionSlotsKeys[i]) return true;
         }
-        for (int i = 0; i < skillSlots.Length; i++)
+        for(int i=0;i<skillSlotsKeys.Length;i++)
         {
+            if (_keycode == skillSlotsKeys[i]) return true;
         }
+        return false;
+    }
+
+    public void SelectKeySetButton()
+    {
+        // 단축키 설정에서 버튼 누르면 실행될 함수
     }
 }
