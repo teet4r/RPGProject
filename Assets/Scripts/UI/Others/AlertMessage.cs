26295705b272e62610f128dd83b32f76c79e08dc
LostMoa <life4458@naver.com>
2022-12-20 22:50:25 +0900

    20221220 update
    이것저것 수정

--- Assets/Scripts/UI/Others/AlertMessage.cs
+++ Assets/Scripts/UI/Others/AlertMessage.cs
@@ -8,15 +8,24 @@ public class AlertMessage : MonoBehaviour
     [SerializeField] float time;
     [SerializeField] float moveSpeed;
     Color color;
+    float[] colorSub = new float[4];
+    [SerializeField] Color purColor;
     private void Start()
     {
         Destroy(this.gameObject, time);
         color = GetComponent<Text>().color;
+        colorSub[0] = purColor.a - color.a;
+        colorSub[1] = purColor.r - color.r;
+        colorSub[2] = purColor.g - color.g;
+        colorSub[3] = purColor.b - color.b;
     }
     private void Update()
     {
         GetComponent<RectTransform>().Translate(new Vector3(0, moveSpeed * Time.deltaTime, 0));
         GetComponent<Text>().color = color;
-        color.a -= Time.deltaTime / time;
+        color.a += Time.deltaTime / time * colorSub[0];
+        color.r += Time.deltaTime / time * colorSub[1];
+        color.g += Time.deltaTime / time * colorSub[2];
+        color.b += Time.deltaTime / time * colorSub[3];
     }
 }
