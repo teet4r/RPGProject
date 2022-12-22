d8ca21e70297338d4feb92df74fdb742011a0b16
LostMoa <life4458@naver.com>
2022-12-22 18:01:08 +0900

    20221222 update
    SkillManager 수정

--- Assets/Scripts/SkillManager.cs
+++ Assets/Scripts/SkillManager.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class SkillManager : MonoBehaviour
 {
+    // 작성자 : 김두현
     public static SkillManager instance;
+
+    Dictionary<SKILLTYPE, bool> skillAble = new();
+
+    public enum SKILLTYPE
+    { SWORD1, SWORD2, AXE1, AXE2, SHIELD1, SHIELD2, ENUM_SIZE }
+
     private void Awake()
     {
         if (instance == null)
@@ -16,5 +23,25 @@ public class SkillManager : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < (int)SKILLTYPE.ENUM_SIZE; i++)
+        {
+            skillAble.Add((SKILLTYPE)i, false);
+        }
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public class SkillInfo
+    {
+        [SerializeField] SkillManager.SKILLTYPE skillType;
+        [SerializeField] float coolTime;
+        [SerializeField] float coolTimeLeft;
     }
 }
