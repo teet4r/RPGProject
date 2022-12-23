da10009528b7ff27dd468e284103f86c4c9e7f8f
LostMoa <life4458@naver.com>
2022-12-23 17:44:11 +0900

    20221223 update
    SkillManager 수정

--- Assets/Scripts/SkillManager.cs
+++ Assets/Scripts/SkillManager.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class SkillManager : MonoBehaviour
@@ -8,11 +7,9 @@ public class SkillManager : MonoBehaviour
     // 작성자 : 김두현
     public static SkillManager instance;
 
-    Dictionary<SKILLTYPE, bool> skillAble = new();
-
     public enum SKILLTYPE
     { SWORD1, SWORD2, AXE1, AXE2, SHIELD1, SHIELD2, ENUM_SIZE }
-    [SerializeField] SkillInfo[] skillInfo = new SkillInfo[(int)SKILLTYPE.ENUM_SIZE];
+    [SerializeField] SkillInfo[] skillInfos;
 
     private void Awake()
     {
@@ -31,7 +28,7 @@ public class SkillManager : MonoBehaviour
     {
         for (int i = 0; i < (int)SKILLTYPE.ENUM_SIZE; i++)
         {
-            skillAble.Add((SKILLTYPE)i, false);
+            skillInfos[i].RefreshSkillCoolTime();
         }
     }
 
@@ -40,16 +37,90 @@ public class SkillManager : MonoBehaviour
 
     }
 
-    public class SkillInfo
+    void CheckSkillExp(SkillInfo _skillInfo)
     {
-        [SerializeField] SkillManager.SKILLTYPE skillType;
-        [SerializeField] Skill skill;
-        [SerializeField] float coolTimeLeft;
-        [SerializeField] string[] skillInfo;
+        while (_skillInfo.CanLevelUp())
+        {
+            _skillInfo.SkillLevelUp();
+        }
     }
 
-    void CheckSkillExp()
+    void RefreshSkillCoolTime()
     {
+        for (int i = 0; i < skillInfos.Length; i++)
+        {
+            if (skillInfos[i].SkillAble)
+            {
+                continue;
+            }
+            else
+            {
+                skillInfos[i].RefreshSkillCoolTime();
+            }
+        }
+    }
 
+    /*
+    public void AddSkillExp(SkillInfo _skillInfo, int _exp)
+    {
+        _skillInfo.AddSkillExp(_exp);
+    }
+    */
+
+    [System.Serializable]
+    private class SkillInfo
+    {
+        [SerializeField] SKILLTYPE skillType;
+        [SerializeField] Skill skill;
+        [SerializeField] float coolTimeLeft = 0;
+        [SerializeField] string[] skillInfos;
+        [SerializeField] string skillInfo;
+        [SerializeField] bool skillAble = false;
+        [SerializeField] bool skillUsable = false;
+        [SerializeField] int skillLevel = 0;
+        [SerializeField] int skillExp = 0;
+
+        public SKILLTYPE SkillType { get { return skillType; } }
+        public Skill Skill { get { return skill; } }
+        public float CoolTimeLeft { get { return coolTimeLeft; } }
+        public bool SkillAble { get { return skillAble; } }
+
+        public void RefreshSkillCoolTime()
+        {
+            if (!SkillAble)
+            {
+                coolTimeLeft -= Time.deltaTime;
+            }
+        }
+
+        public void SetSkillInfo()
+        {
+            skillInfo = skillInfos[skillLevel];
+        }
+
+        public void SkillLevelUp()
+        {
+            skillExp = skillExp - skill.MaxExp[0];
+            skillLevel++;
+        }
+
+        public void SkillExpUp(int _exp)
+        {
+            skillExp += _exp;
+        }
+
+        public bool CanLevelUp()
+        {
+            if (skillExp >= skill.MaxExp[skillLevel])
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public void AddSkillExp(int _exp)
+        {
+            skillExp += _exp;
+        }
     }
 }
