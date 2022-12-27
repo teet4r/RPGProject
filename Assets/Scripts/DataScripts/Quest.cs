37446815a46ffb7a2ffb9a4cb0ae8bb98564f85e
LostMoa <life4458@naver.com>
2022-12-28 07:36:03 +0900

    20221228 update
    Inventory, UI 수정

--- Assets/Scripts/DataScripts/Quest.cs
+++ Assets/Scripts/DataScripts/Quest.cs
@@ -7,7 +7,11 @@ public class Quest : ScriptableObject
 {
     // 작성자 : 김두현
     [SerializeField] string questTitle; // 퀘스트 제목
+    [SerializeField] string questNpc; // 퀘스트 Npc
     [SerializeField] string questInfo; // 퀘스트 설명
+    [SerializeField] string questRequire; // 퀘스트 완료 조건
+
+    [SerializeField] int questCode; // 퀘스트
 
     [SerializeField] QuestPrizeItem[] questPrizeItems; // 퀘스트 보상 아이템 리스트
     [SerializeField] int questPrizeGold; // 퀘스트 보상 골드
