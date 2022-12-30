2256a7ae5cb28984b381e3450c3d8146eb1b911f
damdadi93 <77727581+damdadi93@users.noreply.github.com>
2022-12-30 15:24:39 +0900

    221230
    이동 기본공격 구현

---
+++ Assets/Sangwoo/s_Script/ChracterInfo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChracterInfo : MonoBehaviour
+{
+
+    public float HP = 100f;
+    public float Atk = 10f;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
