cb8356952aec61620d3916437b6f37a646197bb9
0arari0 <110008465+0arari0@users.noreply.github.com>
2023-01-02 20:12:26 +0900

    설정 ui 조금 수정
    22.01.02
    패널 수정했고 클릭 이펙트 구현 중

--- Assets/Scripts/UI/Buttons/MainMenuButtons.cs
+++ Assets/Scripts/UI/Buttons/MainMenuButtons.cs
@@ -1,12 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class MainMenuButtons : MonoBehaviour
 {
     // 작성자 : 김두현
     // 22.12.16 수정 조아라
     [SerializeField] GameObject optionWindow;
+
+    public GameObject circle;
+    public List<GameObject> btns;
+
+    void Start()
+    {
+        foreach(GameObject btn in btns)
+        {
+            btn.GetComponent<Button>().onClick.AddListener(() => clickEffect(btn));
+        }   
+    }
+    void clickEffect(GameObject gameObject)
+    {
+        Instantiate(circle, gameObject.transform.position, quaternion.identity);
+    }
     public void SelectGameStartButton()
     {
         //SceneManager.LoadScene("InGame");
