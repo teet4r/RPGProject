25d39b2442ba444439e61cca59d53cb111f50987
Damdadi93 <swoo567@naver.com>
2023-01-04 14:29:07 +0900

    230104
    에셋 바꾸고 카메라 수정후 다시 애니메이션 손볼꺼임

--- Assets/Sangwoo/s_Script/Movement/Controller.cs
+++ Assets/Sangwoo/s_Script/Movement/Controller.cs
@@ -10,16 +10,26 @@ public class Controller : MonoBehaviour
     public float camSpeed;
     float mouseX;
     float mouseY;
-    float wheel;
+
+    public float rotationCamera;
+    public float wheel;
+
+
+    [Header("Player")]
+    public Transform playerAxis;
+    public Transform player;
+    public float playerSpeed;
+    public Vector3 movement; 
 
 
     void Start()
     {
-        
+        wheel = -10;
+        mouseY = 4;
     }
 
-    
-    void Update()
+
+    void CamMove()
     {
         mouseX += Input.GetAxis("Mouse X");
         mouseY += Input.GetAxis("Mouse Y") * -1;
@@ -30,9 +40,71 @@ public class Controller : MonoBehaviour
             mouseY = 0;
 
         camAxis_Central.rotation = Quaternion.Euler(new Vector3(camAxis_Central.rotation.x
-                                                + mouseY, camAxis_Central.rotation.y + mouseX, 0) * camSpeed);
+                  + mouseY, camAxis_Central.rotation.y + mouseX, 0) * camSpeed);
+    }
 
 
+    void Zoom()
+    {
+        wheel += Input.GetAxis("Mouse ScrollWheel") * 10;
+        rotationCamera = cam.rotation.x.;
+        if (wheel >= -4)
+        {
+            rotationCamera=+24;
+            wheel = -4; //카메라 낮추기 x: 24 
+            
+        }
+            
+        if (wheel <= -12)
+            wheel = -12; //카메라 x
 
+        cam.localPosition = new Vector3(0, 0, wheel);
+    }
+
+    void PlayerMove()
+    {
+        movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        if(movement != Vector3.zero)
+        {
+            playerAxis.rotation = Quaternion.Euler(new Vector3(0, camAxis_Central.rotation.y + mouseX, 0) * camSpeed);
+
+            playerAxis.Translate(movement * Time.deltaTime * playerSpeed);
+
+            player.localRotation = Quaternion.Slerp(player.localRotation, Quaternion.LookRotation(movement), 5 * Time.deltaTime);
+
+            player.GetComponent<Animator>().SetBool("Walk", true);
+        }
+
+        if (movement == Vector3.zero)
+            player.GetComponent<Animator>().SetBool("Walk", false);  
+
+        camAxis_Central.position = new Vector3(player.position.x, player.position.y + 4, player.position.z);
+
+    }
+
+    void Sprint()
+    {
+       
+            //if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))
+            //{
+            //    Playerspeed = 7f;
+            //    anit.SetBool("IsSprint", true);
+            //    Roll2();
+            //}
+            //else if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.W))
+            //{
+            //    moveSpeed = 3.5f;
+            //    anit.SetBool("IsSprint", false);
+
+            //}
+       
+    }
+
+    void Update()
+    {
+        CamMove();
+        Zoom();
+        PlayerMove();
     }
 }
