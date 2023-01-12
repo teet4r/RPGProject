06effbba5384b4b6829856ab813cbdba7b08ffc3
teet4r <76508241+teet4r@users.noreply.github.com>
2023-01-12 14:06:00 +0900

    몬스터 사운드 일단 제외시킴

--- Assets/Scripts/Derived/ChestMonster.cs
+++ Assets/Scripts/Derived/ChestMonster.cs
@@ -4,55 +4,5 @@ using UnityEngine;
 
 public class ChestMonster : NormalMonsterObject
 {
-    protected override IEnumerator _TriggerGetDamage(float damage)
-    {
-        isInvincible = true;
-
-        SoundManager.instance.sfxPlayer.Play(Sfx.BoxyGetDamage);
-        curHp -= damage;
-        if (curHp <= 0f)
-            _Die();
-        else
-            _animator.SetTrigger(AnimatorID.Trigger.Hit);
-
-        yield return _wfs_invincible;
-        isInvincible = false;
-    }
-    protected override void _RushToTarget()
-    {
-        _navMeshAgent.stoppingDistance = data.stoppingDistance;
-        if (hasTarget)
-        {
-            SoundManager.instance.sfxPlayer.Play(Sfx.BoxyWalk);
-            _navMeshAgent.destination = target.transform.position;
-        }
-        else return;
-
-        var lookDir2D = target.transform.position;
-        lookDir2D.y = transform.position.y;
-        transform.LookAt(lookDir2D);
-
-        if (isAttackable && _attackCor == null)
-            _attackCor = StartCoroutine(_Attack());
-    }
-    protected override IEnumerator _Attack()
-    {
-        isAttacking = true;
-
-        SoundManager.instance.sfxPlayer.Play(Sfx.BoxyAttack);
-        int idx = Random.Range(0, _attackClips.Length);
-        _animator.SetTrigger(AnimatorID.Trigger.Attacks[idx]);
-        yield return new WaitForSeconds(_attackClips[idx].length + 1f);
-
-        _navMeshAgent.destination = hasTarget ? target.transform.position : transform.position;
-        isAttacking = false;
-        _attackCor = null;
-    }
-    protected override IEnumerator _DieRoutine()
-    {
-        SoundManager.instance.sfxPlayer.Play(Sfx.BoxyDead);
-        _animator.SetTrigger(AnimatorID.Trigger.Die);
-        yield return new WaitForSeconds(_destroyTime);
-        ObjectPools.instance.normalMonsterPool.Put(gameObject);
-    }
+    
 }
--- Assets/Scripts/Derived/GolemBoss.cs
+++ Assets/Scripts/Derived/GolemBoss.cs
@@ -14,30 +14,11 @@ public class GolemBoss : BossMonsterObject
     {
         _navMeshAgent.stoppingDistance = data.stoppingDistance;
         if (hasTarget)
-        {
-            SoundManager.instance.sfxPlayer.Play(Sfx.BossMove);
             _navMeshAgent.destination = target.transform.position;
-        }
         else return;
 
         if (isAttackable && _attackCor == null)
             _attackCor = StartCoroutine(_Attack());
-    }
-    protected override IEnumerator _TriggerGetDamage(float damage)
-    {
-        isInvincible = true;
-
-        curHp -= damage;
-        if (curHp <= 0f)
-            _Die();
-        else
-        {
-            SoundManager.instance.sfxPlayer.Play(Sfx.BossGetDamage);
-            _animator.SetTrigger(AnimatorID.Trigger.Hit);
-        }
-
-        yield return _wfs_invincible;
-        isInvincible = false;
     }
     protected override IEnumerator _Attack()
     {
@@ -48,7 +29,6 @@ public class GolemBoss : BossMonsterObject
         yield return _rotate3D.StartCoroutine(_rotate3D.Rotate(target.transform.position));
 
         // АјАн
-        SoundManager.instance.sfxPlayer.Play(Sfx.BossAttack);
         int idx = Random.Range(0, _attackClips.Length);
         _animator.SetTrigger(AnimatorID.Trigger.Attacks[idx]);
         _attackPatterns[idx].Attack(this, target.transform);
@@ -61,13 +41,6 @@ public class GolemBoss : BossMonsterObject
         _navMeshAgent.destination = hasTarget ? target.transform.position : transform.position;
         isAttacking = false;
         _attackCor = null;
-    }
-    protected override IEnumerator _DieRoutine()
-    {
-        SoundManager.instance.sfxPlayer.Play(Sfx.BossDead);
-        _animator.SetTrigger(AnimatorID.Trigger.Die);
-        yield return new WaitForSeconds(_destroyTime);
-        ObjectPools.instance.bossMonsterPool.Put(gameObject);
     }
 
     List<IAttackPattern> _attackPatterns = new List<IAttackPattern>();
