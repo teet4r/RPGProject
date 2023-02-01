206721abd6f8072c3cd7acb4715bd151e53f2d2c
teet4r <76508241+teet4r@users.noreply.github.com>
2023-02-01 13:12:20 +0900

    230201 Update
    리소스 파일 정리

--- Assets/Scripts/ItemEffect.cs
+++ Assets/Scripts/ItemEffect.cs
@@ -4,18 +4,11 @@ using UnityEngine;
 
 public class ItemEffect : MonoBehaviour
 {
-    [SerializeField] Rigidbody _rigidbody;
-    [Min(0f)][SerializeField] float _destroyTime = 10f;
+    [SerializeField] Rigidbody _rigidbody = null;
+    [SerializeField] float _addUpwardForce = 10f;
 
     void Start()
     {
-        _rigidbody.AddForce(transform.up * 20f);
-
-        Invoke("_Destroy", _destroyTime);
-    }
-
-    void _Destroy()
-    {
-        Destroy(gameObject);
+        _rigidbody.AddForce(transform.up * _addUpwardForce);
     }
 }
